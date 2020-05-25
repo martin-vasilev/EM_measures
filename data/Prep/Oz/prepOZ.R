1f5726f5cc8797bd73cdae41fc25dec4f24f01e1
Martin R. Vasilev <martin.r.vasilev@gmail.com>
2020-05-25 13:47:55 +0100

    fix OZ data

--- data/Prep/Oz/prepOZ.R
+++ data/Prep/Oz/prepOZ.R
@@ -1,10 +1,29 @@
 
 rm(list= ls())
 
-OZ<- FD[,c("subject", "order", "item", "condition", "line", "wordnum", "word", "FF", "Gaze", "TotalTime", "freq", "Zipf",
-           "word_len", "logFreq")]
+# load/ install preprocssing package:
+if('EMreading' %in% rownames(installed.packages())==FALSE){
+  if('devtools' %in% rownames(installed.packages())==FALSE){
+    install.packages('devtools')
+    library(devtools)
+  }else{
+    library(devtools)
+  }
+  install_github('martin-vasilev/EMreading')
+}else{
+  library(EMreading)
+}
 
-colnames(OZ)<- c("sub", "seq", "item", "cond", "line", "word", "wordID", "FFD", "GD", "TVT",
-                 "freq", "Zipf", "word_len", "logFreq")
+load("data/Prep/Oz/Bold_data_raw.Rda")
+
+OZ<- wordMeasures(raw_OZ)
+
+OZ$RS<- NULL
+OZ$RS_type<- NULL
+OZ<- OZ[-which(OZ$blinks_1stPass==1 | OZ$blinks_2ndPass==1),]
+
+OZ$blinks_1stPass<- NULL
+OZ$blinks_2ndPass<- NULL
+
 
 save(OZ, file= "data/OZ.Rda")
