d0c11cb389cb5620cd5b6f1947c7788d931e0c25
Martin R. Vasilev <martin.r.vasilev@gmail.com>
2025-06-14 11:29:06 +0100

    upload some initial code

--- Analysis.R
+++ Analysis.R
@@ -1,32 +1,51 @@
 
-# Martin R. Vasilev, 2019-2020
+# Martin R. Vasilev, 2025
 
 rm(list= ls())
 
 load("data/Provo.Rda") # Provo corpus
 load("data/geco.Rda") # GECO corpus
 
-### Check data:
 
-Provo2<- subset(Provo, !is.na(FFD)) # get rid of NAs for FFD and GD (only remaining NAs for SFD)
-Provo3<- subset(Provo2, !is.na(SFD)) # remove NAs for SFD as well
+# colorblind palletes: # https://venngage.com/blog/color-blind-friendly-palette/
+pallete1= c("#CA3542", "#27647B", "#849FA0", "#AECBC9", "#57575F") # "Classic & trustworthy"
 
-# check just to make sure:
-table(is.na(Provo3$FFD))
-table(is.na(Provo3$SFD))
-table(is.na(Provo3$GD))
-table(is.na(Provo3$GPT))
+library(ggplot2)
+library(tidyverse)
+library(ggcorrplot)
+library(ggpubr)
 
-cor(x=Provo2$FFD, y=Provo2$GD) # 0.5950107
-cor(x=Provo2$FFD, y=Provo2$TVT) # 0.4057805
-cor(x=Provo2$FFD, y=Provo2$GPT) # 0.1609874
+geco.c<- subset(geco, nfixAll<100)
+geco.c<- geco.c[,c(10, 15, 11, 12, 13, 14)]
 
-cor(x=Provo2$GD, y=Provo2$TVT) # 0.7042744
-cor(x=Provo2$TVT, y=Provo2$GPT) # 0.4707271
+colnames(geco.c)<- c("FFD", "SFD", "GD", "GPT", "TFT","Skip" )
 
-library(lme4)
-L1<- lmer(log(FFD)~ cloze_model +(cloze_model|sub)+ (1|item), data= Provo2)
-summary(L1)
+r_corr<- cor(geco.c, use = 'pairwise.complete.obs', method = 'pearson', )
+
+P1<-r_corr%>%ggcorrplot(method = 'square', type = 'upper',
+                    title = 'GECO (Cop et al., 2017)', digits = 2, show.diag = F,
+                    outline.color = 'black', lab = T)
+
+
+Provo_c<- Provo[,c(9, 11, 10,13, 12, 14)]
+colnames(Provo_c)<- c("FFD", "SFD", "GD", "GPT", "TFT","Skip" )
+
+r_corr2<- cor(Provo_c, use = 'pairwise.complete.obs', method = 'pearson')
+
+P2<-r_corr2%>%ggcorrplot(method = 'square', type = 'upper',
+                        title = 'Provo (Luke & Christianson, 2018)', digits = 2, show.diag = F,
+                        outline.color = 'black', lab = T)
+
+figure1 <- ggarrange(P1, P2, ncol = 2)
+
+ggsave(filename = 'Plots/Corr_plot.pdf', plot = figure1,
+       width = 8, height = 4)
+
+
+### First-pass refixation probability:
+
+
+
 
 
 
